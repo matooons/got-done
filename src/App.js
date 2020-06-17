6442d35ed9ed3d2046efaedfc1c6dc1adf3e1dc6
matthew-lebaron <matthewlebaron10@gmail.com>
2020-06-16 17:29:29 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="App">
+        BIG FAT FUCKING ANIME TIDDY
+        <div class="accomplishments">
+          <textarea
+            name="message"
+            id="message"
+            placeholder="accomplishments"
+          ></textarea>
+        </div>
+        <div class="inconveniences">
+          <textarea
+            name="message"
+            id="message"
+            placeholder="inconveniences"
+          ></textarea>
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
